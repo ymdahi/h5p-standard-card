28d84dd07aa4fc5fef9d9857c78bb12096a3e710
ydahi <ymdahi@gmail.com>
2019-11-21 11:31:52 -0500

    clean up for next steps

--- scripts/standard-card.js
+++ scripts/standard-card.js
@@ -14,32 +14,22 @@ H5P.StandardCard = (function ($) {
     // Keep provided id.
     this.id = id;
 
-    console.log(`from standardcard 0: ${this.options}`);
   };
  
-  console.log(`from standardcard 1: ${this.options}`);
-
   /**
    * Attach function called by H5P framework to insert H5P content into
    * page
    *
    * @param {jQuery} $container
    */
-  C.prototype.attach = function ($container) {
-    // Set class on container to identify it as a greeting card
-    // container.  Allows for styling later.
-    $container.addClass("h5p-standard-card");
-    // Add image if provided.
-    if (this.options.cardImage && this.options.cardImage.path) {
-      $container.append('<img class="card-image" src="' + H5P.getPath(this.options.cardImage.path, this.id) + '">');
-    }
-    // Add greeting text.
-    $container.append('<div class="card-text">' + this.options.cardTitle + '</div>');
 
-    console.log(`from standardcard 2: ${this.options}`);
+  StandandCard.prototype.attach = function ($container) {
+
+    $container.addClass("h5p-standard-card");
+    console.log("from standard-card.js");
   };
  
-  return C;
+  return StandandCard;
 
 
 })(H5P.jQuery);
